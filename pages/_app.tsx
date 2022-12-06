bec7c19f47d24907133e71c14e960510140b2909
tomosia_thien_tran <thien.tran@tomosia.com>
2022-12-06 16:44:20 +0700

    Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@fontsource/roboto"
+import '../styles/main.scss'
+import Layout from '../components/Layout/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Layout><Component {...pageProps} /></Layout>
 }
